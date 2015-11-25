1a81d718dab651d61d3e0c0ae97b8fa550575aea
Daniel Qin <taurencoder@gmail.com>
2015-11-25 16:59:06 +0800

    updated startup script

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const bodyparser = require('koa-body');
 app.use(convert(bodyparser({multipart: true})));
 app.use(convert(log.middleware(log.logger, {level: 'auto'})));
 
-app.use(ctx => {
-  ctx.body = 'test';
+app.use(async (ctx, next) => {
+  const start = new Date;
+  await next();
+  const ms = new Date - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
 module.exports = app;
